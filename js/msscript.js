d86ce1d3dad6017b29ce7da4ffb73a3a298b033c
SionPark <sopark126@github.com>
2019-07-08 15:13:53 -0700

    Removed js folder

--- js/msscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-     $(".nav_icon").click(function(){
-            $("nav").slideToggle("on");  
-            $("article").toggleClass("on");
-            $(".navicon").toggleClass("close" ,1000);
-    });
-});
-
